56379945f8df12ae26297209fe76c1701ce4effc
John Zhang <zhangdijohn@gmail.com>
2019-11-21 17:08:21 +1100

    setp one metapath2vec similarity

---
+++ freshgraph/candidating.py
@@ -0,0 +1,74 @@
+from stellargraph.data import UniformRandomMetaPathWalk
+from stellargraph import StellarGraph
+from gensim.models import Word2Vec
+import collections
+
+METAPATHS = [
+    ["movie", "genre", "movie"],
+    ["movie", "actor", "movie"],
+    ["movie", "director", "movie"],
+]
+
+WALK_DISTANCE = 100
+
+
+def _metapath_randomwalk(graph):
+    # Create the random walker
+    rw = UniformRandomMetaPathWalk(StellarGraph(graph))
+
+    # specify the metapath schemas as a list of lists of node types.
+
+    walks = rw.run(nodes=list(graph.nodes()),  # root nodes
+                   length=WALK_DISTANCE,  # maximum length of a random walk
+                   n=1,        # number of random walks per root node
+                   metapaths=METAPATHS  # the metapaths
+                   )
+
+    print("Number of random walks: {}".format(len(walks)))
+
+    return walks
+
+
+def _metapath2vec(metapath_walk):
+    model = Word2Vec(metapath_walk, size=128, window=5,
+                     min_count=0, sg=1, workers=4, iter=10)
+    return model
+
+
+def get_candidates_model(graph):
+    return _metapath2vec(_metapath_randomwalk(graph))
+
+
+def get_similar_items(model, source_id):
+    # NOTE: consider refactoring the source_id
+    # source_id = 3884
+    similar_nodes = model.most_similar(f'm_{source_id}', topn=100)
+    # similar_ids = [key[2:] for key, _ in similar_nodes if key.startswith('m_')]
+    item_scores_dict = {key[2:]: value for key,
+                        value in similar_nodes if key.startswith('m_')}
+
+    return item_scores_dict
+
+
+def sort_similar_item_by_score(item_scores_dict):
+    sorted_item = [(x[0], x[1]) for x in sorted(
+        item_scores_dict.items(), key=lambda kv: kv[1], reverse=True)]
+    return sorted_item
+
+    # validate similarity
+    # movie_meta.loc[movie_meta['id'].isin(similar_ids)]
+
+
+def get_ranked_user(user_item_tuple, item_scores_dict):
+    similar_item_ids = list(item_scores_dict.keys())
+    connected_users = user_item_tuple.loc[user_item_tuple['movieID'].isin(
+        similar_item_ids)]
+    user_scores = collections.defaultdict(float)
+    for _, row in connected_users.iterrows():
+        user_scores[str(int(row['userID']))
+                    ] += item_scores_dict[str(int(row['movieID']))]
+    return [(x[0], x[1]) for x in sorted(user_scores.items(),
+                                         key=lambda kv: kv[1], reverse=True)]
+
+    # ranked_candidate_item_pair = [
+    #     [int(x[0]), validate_id] for x in sorted_users]
---
+++ freshgraph/data_loader.py
@@ -0,0 +1,77 @@
+import pandas
+import networkx as nx
+
+data_root_folder = "./data"
+
+movie_directors = pandas.read_csv(
+    f"{data_root_folder}/movie_directors.dat", sep="\t", encoding="ISO-8859-1")
+movie_genres = pandas.read_csv(
+    f"{data_root_folder}/movie_genres.dat", sep="\t", encoding="ISO-8859-1")
+movie_actors = pandas.read_csv(
+    f"{data_root_folder}/movie_actors.dat", sep="\t", encoding="ISO-8859-1")
+movie_meta = pandas.read_csv(
+    f"{data_root_folder}/movies.dat", sep="\t", encoding="ISO-8859-1")
+# top 5 actors of each movie
+lead_actors = movie_actors[movie_actors['ranking'] <= 5]
+
+# training set
+movies_train = movie_meta[movie_meta['year'] < 2009]
+# validation set 29 movies
+movies_validate = movie_meta[movie_meta['year'] >= 2009]
+
+# user movie interactions
+movie_users = pandas.read_csv(
+    f"{data_root_folder}/user_ratedmovies.dat", sep="\t", encoding="ISO-8859-1")
+positive_ratings = movie_users[movie_users['rating'] > 3]
+negative_ratings = movie_users[movie_users['rating'] < 4]
+
+# exclude validation user-movie tuples
+user_item_tuple_train = positive_ratings[~positive_ratings['movieID'].isin(
+    movies_validate['id'].values)].filter(['userID', 'movieID'], axis=1)
+
+
+user_node_ids = ["u_" + str(user_node_id)
+                 for user_node_id in set(positive_ratings['userID'].values)]
+movie_node_ids = ["m_" + str(user_node_id)
+                  for user_node_id in set(positive_ratings['movieID'].values)]
+
+# pos_movie_user_edges = [("m_" + str(row['movieID']), "u_" + str(row['userID']))
+#                         for _, row in positive_ratings.iterrows()]
+
+
+def get_graph():
+    movie_node_ids = ["m_" + str(movie_id)
+                      for movie_id in set(movie_meta['id'].values)]
+
+    genres_node_ids = ["g_" + str(genre_id)
+                       for genre_id in set(movie_genres['genre'].values)]
+    movie_genre_edges = [("m_" + str(row['movieID']), "g_" + str(row['genre']))
+                         for _, row in movie_genres.iterrows()]
+
+    actors_node_ids = ["a_" + str(actor_id)
+                       for actor_id in set(lead_actors['actorID'].values)]
+    movie_actor_edges = [("m_" + str(row['movieID']), "a_" +
+                          str(row['actorID'])) for _, row in lead_actors.iterrows()]
+
+    directors_node_ids = [
+        "d_" + str(director_id) for director_id in set(movie_directors['directorID'].values)]
+    movie_director_edges = [("m_" + str(row['movieID']), "d_" + str(row['directorID']))
+                            for _, row in movie_directors.iterrows()]
+
+    g_nx = nx.Graph()  # create the graph
+
+    g_nx.add_nodes_from(movie_node_ids, label="movie")
+
+    g_nx.add_nodes_from(genres_node_ids, label="genre")
+    g_nx.add_edges_from(movie_genre_edges, label="movie_genre")
+
+    g_nx.add_nodes_from(actors_node_ids, label="actor")
+    g_nx.add_edges_from(movie_actor_edges, label="movie_actor")
+
+    g_nx.add_nodes_from(directors_node_ids, label="director")
+    g_nx.add_edges_from(movie_director_edges, label="movie_director")
+
+    print("Number of nodes {} and number of edges {} in graph.".format(
+        g_nx.number_of_nodes(), g_nx.number_of_edges()))
+
+    return g_nx
